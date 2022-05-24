18f84a2ba2e05ded5130a957d3e72ae6ea84dfde
Ivan-Laptiev <laptiev.ivan.j@gmail.com>
2022-05-24 13:25:25 +0300

    Друга та третя задачі

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const loaded = localStorage.getItem('videoplayer-current-time');
+
+if (loaded) {
+  player.setCurrentTime(loaded);
+}
+
+function onCurrentTime(seconds) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
